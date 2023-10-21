96884edcfa964e49fcdd4a8c4210aa4916805af1
tuNguyenGithub <tu.nguyen@egany.com>
2023-10-21 15:09:42 +0700

    Done Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
+import { CaretDownIcon } from '@radix-ui/react-icons'
 import Navigation from '../Navigation'
 
 const Header = () => {
   return (
-    <div className="sticky top-0 left-0 width-0 h-[5.25rem] bg-[rgba(23,22,26,0.7)] z-10" >
-      <div className='container'>
-      <Navigation />
-      <a className='button bg-primary'>Connect Wallet</a>
+    <div className="sticky top-0 left-0 width-0 h-[5.25rem] bg-[rgba(23,22,26,0.7)] z-10">
+      <div className="container flex w-full justify-between items-center h-full">
+        <div className='flex'>
+          <div className="w-32"></div>
+          <Navigation />
+        </div>
+        <div className="flex gap-x-11">
+          <a href="/" className="button bg-primary">
+            Connect Wallet
+          </a>
+          <a href="/" className="flex gap-x-4 items-center">
+            <img src="globe.svg" width={16} height={16} alt="lang" />
+            <CaretDownIcon className="w-5 h-5" />
+          </a>
+        </div>
       </div>
-      
     </div>
   )
 }
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -7,20 +7,23 @@ type NavigationType = {
 
 const Navigation = () => {
   return (
-    <div>
-      <ul>
-        {navigation.map((nav, index) => (
-          <NavigationItem key={index} item={nav} />
-        ))}
-      </ul>
-    </div>
+    <ul className="flex gap-x-[3.75rem]">
+      {navigation.map((nav, index) => (
+        <NavigationItem key={index} item={nav} />
+      ))}
+    </ul>
   )
 }
 
 const NavigationItem = ({ item }: { item: NavigationType }) => {
   return (
     <li>
-      <a href={item.url}>{item.text}</a>
+      <a
+        href={item.url}
+        className="uppercase text-sm font-bold hover:text-primary transition-[color] duration-300"
+      >
+        {item.text}
+      </a>
     </li>
   )
 }
