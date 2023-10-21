7d208bbd44127bfb32d3c6c2586d66b011bd995f
tuNguyenGithub <tu.nguyen@egany.com>
2023-10-21 14:51:47 +0700

    WIP: Build header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,15 @@
-import Navigation from "../Navigation"
+import Navigation from '../Navigation'
 
 const Header = () => {
-  return <div>
-    <Navigation />
+  return (
+    <div className="sticky top-0 left-0 width-0 h-[5.25rem] bg-[rgba(23,22,26,0.7)] z-10" >
+      <div className='container'>
+      <Navigation />
+      <a className='button bg-primary'>Connect Wallet</a>
+      </div>
+      
     </div>
+  )
 }
 
 export default Header
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,5 +1,28 @@
+import navigation from '../../mock-data/navigation'
+
+type NavigationType = {
+  text: string
+  url: string
+}
+
 const Navigation = () => {
-  return <></>
+  return (
+    <div>
+      <ul>
+        {navigation.map((nav, index) => (
+          <NavigationItem key={index} item={nav} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const NavigationItem = ({ item }: { item: NavigationType }) => {
+  return (
+    <li>
+      <a href={item.url}>{item.text}</a>
+    </li>
+  )
 }
 
 export default Navigation
